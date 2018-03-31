ea833816ac9c46c6298ff0ecde1a288c7e2d9315
John Tam <john@trinitymobilenetworks.com>
2018-03-31 15:22:45 -0400

    Completed friend request functions. Changed formatting.

--- contract/BitAccount.sol
+++ contract/BitAccount.sol
@@ -5,86 +5,117 @@ pragma solidity ^0.4.21;
 // The private information is encrypted
 contract BitsAccount {
 
-	struct Bit {
-		uint timestamp;
-		string bitString;
-	}
-
-	// Public tweets
-	mapping (uint => Bit) public publicBits;
-	uint public numPublicBits;
-
-	// Private tweets
-	mapping (uint => Bit) public privateBits;
-	uint public numPrivateBits;
-
-	// Friends list
-  address[] friends;
-
-	// Only the owner/user should be allowed to tweet
-	address owner;
-
-	// Password encrypted with the user's private key
-	bytes32 encryptedPassword;
-
-	// Takes in a password encrypted with the user's private key
-	function BitsAccount(bytes32 _encryptedPassword) public {
-		numPublicBits = 0;
-		numPrivateBits = 0;
-		owner = msg.sender;
-		encryptedPassword = _encryptedPassword;
-	}
-
-	// Modifier to ensure only the owner can control his account
-	modifier onlyOwner() {
-		require(msg.sender == owner);
-		_;
-	}
-
-	// Create new public bit
-	function postPublicBit(string _bitString) public onlyOwner {
-		require (bytes(_bitString).length <= 160);
-
-		publicBits[numPublicBits].timestamp = now;
-		publicBits[numPublicBits].bitString = _bitString;
-		numPublicBits++;
-	}
-
-	// Create new private (encrypted) bit
-	function postPrivateBit(string _encryptedBitString) public onlyOwner {
-		require (bytes(_encryptedBitString).length <= 160);
-
-		privateBits[numPrivateBits].timestamp = now;
-		privateBits[numPrivateBits].bitString = _encryptedBitString;
-		numPrivateBits++;
-	}
-
-	// Get a public bit by ID
-	function getPublicBit(uint _bitId) public constant returns (string bitString, uint timestamp) {
-		bitString = publicBits[_bitId].bitString;
-		timestamp = publicBits[_bitId].timestamp;
-	}
-
-	// Get a private bit by ID
-	function getPrivateBit(uint _bitId) public constant returns (string bitString, uint timestamp) {
-		bitString = privateBits[_bitId].bitString;
-		timestamp = privateBits[_bitId].timestamp;
-	}
-
-	// Get the latest public bit
-	function getLatestPublicBit() public constant returns (string bitString, uint timestamp) {
-		bitString = publicBits[numPublicBits - 1].bitString;
-		timestamp = publicBits[numPublicBits - 1].timestamp;
-	}
-
-	// Get the latest private bit
-	function getLatestPrivateBit() public constant returns (string encryptedBitString, uint timestamp) {
-		encryptedBitString = privateBits[numPrivateBits - 1].bitString;
-		timestamp = privateBits[numPrivateBits - 1].timestamp;
+  struct Bit {
+    uint timestamp;
+    string bitString;
   }
 
-	// Add an address to friends list
-  function addToFriendsList(address friend) public onlyOwner {
-  	friends.push(friend);
+  // Struct -- each friend is associated with
+  // an address (contract address) and they are
+  // associated with a password that is encrypted
+  // with this contract's own private key.
+  struct Friend {
+    address addr;
+    string encryptedPw;
+  }
+
+  // Friends list
+  mapping (address => Friend) friends;
+  mapping (address => Friend) pending;
+
+  // Public tweets
+  mapping (uint => Bit) public publicBits;
+  uint public numPublicBits;
+
+  // Private tweets
+  mapping (uint => Bit) public privateBits;
+  uint public numPrivateBits;
+
+  // Only the owner/user should be allowed to tweet
+  address owner;
+
+  // Password encrypted with the user's private key
+  bytes32 encryptedPassword;
+
+  // Takes in a password encrypted with the user's private key
+  function BitsAccount(bytes32 _encryptedPassword) public {
+    numPublicBits = 0;
+    numPrivateBits = 0;
+    owner = msg.sender;
+    encryptedPassword = _encryptedPassword;
+  }
+
+  // Modifier to ensure only the owner can control his account
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  // Create new public bit
+  function postPublicBit(string _bitString) public onlyOwner {
+    require (bytes(_bitString).length <= 160);
+
+    publicBits[numPublicBits].timestamp = now;
+    publicBits[numPublicBits].bitString = _bitString;
+    numPublicBits++;
+  }
+
+  // Create new private (encrypted) bit
+  function postPrivateBit(string _encryptedBitString) public onlyOwner {
+    require (bytes(_encryptedBitString).length <= 160);
+
+    privateBits[numPrivateBits].timestamp = now;
+    privateBits[numPrivateBits].bitString = _encryptedBitString;
+    numPrivateBits++;
+  }
+
+  // Get a public bit by ID
+  function getPublicBit(uint _bitId) public constant returns (string bitString, uint timestamp) {
+    bitString = publicBits[_bitId].bitString;
+    timestamp = publicBits[_bitId].timestamp;
+  }
+
+  // Get a private bit by ID
+  function getPrivateBit(uint _bitId) public constant returns (string bitString, uint timestamp) {
+    bitString = privateBits[_bitId].bitString;
+    timestamp = privateBits[_bitId].timestamp;
+  }
+
+  // Get the latest public bit
+  function getLatestPublicBit() public constant returns (string bitString, uint timestamp) {
+    bitString = publicBits[numPublicBits - 1].bitString;
+    timestamp = publicBits[numPublicBits - 1].timestamp;
+  }
+
+  // Get the latest private bit
+  function getLatestPrivateBit() public constant returns (string encryptedBitString, uint timestamp) {
+    encryptedBitString = privateBits[numPrivateBits - 1].bitString;
+    timestamp = privateBits[numPrivateBits - 1].timestamp;
+  }
+
+  // Accept a friend request; adds to the list.
+  function receiveFriendRequest(address requester, string pw) public returns (bool) {
+    Friend memory friend = Friend(requester, pw);
+    pending[requester] = friend;
+    return true;
+  }
+
+  // Send a friend request to a specific address. The pw
+  // must be encrypted with the TARGET's public key.
+  function sendFriendRequest(address friendAddress, string pw) onlyOwner public returns (bool) {
+    BitsAccount acct = BitsAccount(friendAddress);
+    bool successful = acct.receiveFriendRequest(msg.sender, pw);
+    require(successful);
+    return true;
+  }
+
+  // Accept a friend request. Only the owner can do this.
+  function acceptFriendRequest(address acceptAddress) onlyOwner public returns (bool) {
+    Friend storage pendingFriend = pending[acceptAddress];
+    if (pendingFriend.addr != 0) {   // If address exists, good to go.
+      friends[acceptAddress] = pendingFriend;
+      return true;
+    }
+    return false;
   }
 }
